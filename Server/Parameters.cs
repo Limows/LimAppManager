2e0391fb54bc470afc3f263b4f712d0dc7dbe756
Limows DigitalDemon <limows@limpad.limowski.xyz>
2021-12-12 13:58:42 +0300

    Server init commit

---
+++ Server/Parameters.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace LimAppManager
+{
+    public class Parameters
+    {
+        static public string Uri;
+        static public int Requests;
+        static public int Logins;
+        static public int Downloads;
+        static public int Uploads;
+    }
+}
